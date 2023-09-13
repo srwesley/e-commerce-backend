e990fc4e5a23054e4d0314fcf8cfc062b5bb36fc
Sarah Wesley <srwesley722@gmail.com>
2023-09-13 14:03:11 -0500

    Added server.js file

---
+++ server.js
@@ -0,0 +1,19 @@
+// Dependencies
+const express = require("express");
+const routes = require("./routes");
+
+// Import sequelize connection
+const sequelize = require("./config/connection");
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use(routes);
+
+// Sync sequelize models to the database, then turn on the server
+sequelize.sync().then(() => {
+    app.listen(PORT, () => console.log(`App listening on port ${PORT}!`));
+});
